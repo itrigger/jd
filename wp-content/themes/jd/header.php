9bf80306746d3260985a010376396d33d79a5069
Konstantin <trigger.kst@gmail.com>
2020-03-17 18:28:50 +0600

    next

--- wp-content/themes/jd/header.php
+++ wp-content/themes/jd/header.php
@@ -13,175 +13,53 @@
 
 <html class="no-js" <?php language_attributes(); ?>>
 
-	<head>
+<head>
 
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-		<?php wp_head(); ?>
+    <?php wp_head(); ?>
 
-	</head>
+    <link rel="stylesheet" href="/wp-content/themes/jd/assets/css/screen.css">
+</head>
 
-	<body <?php body_class(); ?>>
+<body <?php body_class(); ?>>
 
-		<?php
-		wp_body_open();
-		?>
+<?php
+wp_body_open();
+?>
 
-		<header id="site-header" class="header-footer-group" role="banner">
+<header id="site-header" class="site-header">
+    <div class="site-header_line1">
+        <div class="header-logo-wrap">
+            <a href="/" class="header-logo-wrap__logo"></a>
+        </div>
+        <div class="header-slogan">
+            опыт. ответственность. оперативность
+        </div>
 
-			<div class="header-inner section-inner">
+        <div class="header-contact-wrap">
+            <div class="tel"><a href="tel:+74953692133">+7 495 369 21 33</a></div>
+            <div class="mail"><a href="mailto:info@rvgn.ru">info@rvgn.ru</a></div>
+            <div class="header-contact-wrap__btn-w">
+                <a href="#" class="btn btn-solid">Заказать звонок</a>
+                <a href="#" class="btn btn-alpha">Задать вопрос</a>
+            </div>
+        </div>
+    </div>
+    <div class="site-header_line2">
+        <div class="menu-top">
+            <ul class="menu">
+                <li class="menu__item"><a href="/about">О компании</a></li>
+                <li class="menu__item"><a href="/services">Услуги</a></li>
+                <li class="menu__item"><a href="/info">Информация</a></li>
+                <li class="menu__item"><a href="/news">Новости</a></li>
+                <li class="menu__item"><a href="/reviews">Отзывы</a></li>
+                <li class="menu__item"><a href="/contacts">Контакты</a></li>
+            </ul>
+        </div>
+    </div>
+</header><!-- #site-header -->
 
-				<div class="header-titles-wrapper">
-
-					<?php
-
-					// Check whether the header search is activated in the customizer.
-					$enable_header_search = get_theme_mod( 'enable_header_search', true );
-
-					if ( true === $enable_header_search ) {
-
-						?>
-
-						<button class="toggle search-toggle mobile-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
-							<span class="toggle-inner">
-								<span class="toggle-icon">
-									<?php twentytwenty_the_theme_svg( 'search' ); ?>
-								</span>
-								<span class="toggle-text"><?php _e( 'Search', 'twentytwenty' ); ?></span>
-							</span>
-						</button><!-- .search-toggle -->
-
-					<?php } ?>
-
-					<div class="header-titles">
-
-						<?php
-							// Site title or logo.
-							twentytwenty_site_logo();
-
-							// Site description.
-							twentytwenty_site_description();
-						?>
-
-					</div><!-- .header-titles -->
-
-					<button class="toggle nav-toggle mobile-nav-toggle" data-toggle-target=".menu-modal"  data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
-						<span class="toggle-inner">
-							<span class="toggle-icon">
-								<?php twentytwenty_the_theme_svg( 'ellipsis' ); ?>
-							</span>
-							<span class="toggle-text"><?php _e( 'Menu', 'twentytwenty' ); ?></span>
-						</span>
-					</button><!-- .nav-toggle -->
-
-				</div><!-- .header-titles-wrapper -->
-
-				<div class="header-navigation-wrapper">
-
-					<?php
-					if ( has_nav_menu( 'primary' ) || ! has_nav_menu( 'expanded' ) ) {
-						?>
-
-							<nav class="primary-menu-wrapper" aria-label="<?php esc_attr_e( 'Horizontal', 'twentytwenty' ); ?>" role="navigation">
-
-								<ul class="primary-menu reset-list-style">
-
-								<?php
-								if ( has_nav_menu( 'primary' ) ) {
-
-									wp_nav_menu(
-										array(
-											'container'  => '',
-											'items_wrap' => '%3$s',
-											'theme_location' => 'primary',
-										)
-									);
-
-								} elseif ( ! has_nav_menu( 'expanded' ) ) {
-
-									wp_list_pages(
-										array(
-											'match_menu_classes' => true,
-											'show_sub_menu_icons' => true,
-											'title_li' => false,
-											'walker'   => new TwentyTwenty_Walker_Page(),
-										)
-									);
-
-								}
-								?>
-
-								</ul>
-
-							</nav><!-- .primary-menu-wrapper -->
-
-						<?php
-					}
-
-					if ( true === $enable_header_search || has_nav_menu( 'expanded' ) ) {
-						?>
-
-						<div class="header-toggles hide-no-js">
-
-						<?php
-						if ( has_nav_menu( 'expanded' ) ) {
-							?>
-
-							<div class="toggle-wrapper nav-toggle-wrapper has-expanded-menu">
-
-								<button class="toggle nav-toggle desktop-nav-toggle" data-toggle-target=".menu-modal" data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
-									<span class="toggle-inner">
-										<span class="toggle-text"><?php _e( 'Menu', 'twentytwenty' ); ?></span>
-										<span class="toggle-icon">
-											<?php twentytwenty_the_theme_svg( 'ellipsis' ); ?>
-										</span>
-									</span>
-								</button><!-- .nav-toggle -->
-
-							</div><!-- .nav-toggle-wrapper -->
-
-							<?php
-						}
-
-						if ( true === $enable_header_search ) {
-							?>
-
-							<div class="toggle-wrapper search-toggle-wrapper">
-
-								<button class="toggle search-toggle desktop-search-toggle" data-toggle-target=".search-modal" data-toggle-body-class="showing-search-modal" data-set-focus=".search-modal .search-field" aria-expanded="false">
-									<span class="toggle-inner">
-										<?php twentytwenty_the_theme_svg( 'search' ); ?>
-										<span class="toggle-text"><?php _e( 'Search', 'twentytwenty' ); ?></span>
-									</span>
-								</button><!-- .search-toggle -->
-
-							</div>
-
-							<?php
-						}
-						?>
-
-						</div><!-- .header-toggles -->
-						<?php
-					}
-					?>
-
-				</div><!-- .header-navigation-wrapper -->
-
-			</div><!-- .header-inner -->
-
-			<?php
-			// Output the search modal (if it is activated in the customizer).
-			if ( true === $enable_header_search ) {
-				get_template_part( 'template-parts/modal-search' );
-			}
-			?>
-
-		</header><!-- #site-header -->
-
-		<?php
-		// Output the menu modal.
-		get_template_part( 'template-parts/modal-menu' );
---
+++ wp-content/themes/jd/mainpage.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Команда
+*/
+
+get_header();
+?>
+
+<div class="wrapper">
+
+</div>
+
+<?php
+get_footer();
