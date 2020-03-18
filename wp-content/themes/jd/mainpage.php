015f99e96b6d62385d69658056f16af02c45f2b0
Konstantin <trigger.kst@gmail.com>
2020-03-18 18:51:42 +0600

    next

--- wp-content/themes/jd/mainpage.php
+++ wp-content/themes/jd/mainpage.php
@@ -6,23 +6,62 @@ Template Name: Главная страница
 get_header();
 ?>
 
-<div class="wrapper">
-    <div class="block-1">
-        <div class="in">
-            <h1>Регистрация железнодорожного подвижного состава</h1>
-            <div class="block-1-video">
-                <div class="btn-video">
-                    <a href="#"></a>
+    <div class="wrapper">
+        <div class="block-1">
+            <div class="in">
+                <h1>Регистрация железнодорожного подвижного состава</h1>
+                <div class="block-1-video">
+                    <a href="#" class="btn-video"></a>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="block-2">
-        <div class="in">
-
+        <div class="block-2">
+            <div class="in">
+                <div class="pattern-block">
+                    <div class="pattern-block__in">
+                        <div class="pattern-form">
+                            <header class="pattern-block__header">Получите на почту коммерческое предложение без звонка</header>
+                            <div class="flex-wrap">
+                                <div class="form">
+                                    <div class="form-input">
+                                        <input type="text" placeholder="Имя"/>
+                                    </div>
+                                    <div class="form-input">
+                                        <input type="text" placeholder="Телефон"/>
+                                    </div>
+                                    <div class="form-input">
+                                        <input type="text" placeholder="Email"/>
+                                    </div>
+                                    <div class="form-input">
+                                        <input type="submit" value="Получить"/>
+                                    </div>
+                                </div>
+                                <div class="label-conf">Все данные защищены <span>и не передаются третьим лицам</span></div>
+                            </div>
+                        </div>
+                        <div class="quote-block"></div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
     </div>
-</div>
 
 <?php
 get_footer();
