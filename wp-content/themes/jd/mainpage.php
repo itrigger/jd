851d4240b8e682a0a531ddfd11fb0926f71ca247
Konstantin <trigger.kst@gmail.com>
2020-03-19 18:42:49 +0600

    next

--- wp-content/themes/jd/mainpage.php
+++ wp-content/themes/jd/mainpage.php
@@ -68,16 +68,60 @@ get_header();
             </div>
         </div>
         <div class="block-3">
+            <div class="in">
+                <h2>Три шага регистрации подвижного состава</h2>
+            </div>
+            <div class="mp-block-desc">
+                <div class="in">
+                    <div class="mp-block-desc__block">
+                        <div class="mp-block-desc__header mp-block-desc__header_num num1">Начало регистрации</div>
+                        <div class="mp-block-desc__desc">Консультирование. Подготовка и отправка документов в
+                            электронном
+                            виде на сервер компании ЖД Учет
+                        </div>
+                        <a href="#" class="btn btn-yellow">Получить консультацию</a>
+                    </div>
+                    <div class="mp-block-desc__block">
+                        <div class="mp-block-desc__block__image"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="mp-block-desc mp-block-desc__reverse">
+                <div class="in">
+                    <div class="mp-block-desc__block">
+                        <div class="mp-block-desc__header mp-block-desc__header_num num2">Работа с документами</div>
+                        <div class="mp-block-desc__desc">Проводится предварительная проверка документов специалистами ЖД
+                            Учет с передачей комплекта в ответственные органы для отработки и внесения информации в
+                            информационные системы «РЖД».
+                        </div>
+                        <a href="#" class="btn btn-yellow">Получить консультацию</a>
+                    </div>
+                    <div class="mp-block-desc__block">
+                        <div class="mp-block-desc__block__image"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="mp-block-desc">
+                <div class="in">
+                    <div class="mp-block-desc__block">
+                        <div class="mp-block-desc__header mp-block-desc__header_num num3">Получите результаты</div>
+                        <div class="mp-block-desc__desc">По итогам проведения регистрации Заявителю направляются
+                            подтверждающие документы
+                        </div>
+                        <a href="#" class="btn btn-yellow">Получить консультацию</a>
 
+                        <div class="hint mp-block-desc__hint">На всех этапах документы заверяются электронной цифровой
+                            подписью
+                        </div>
+                    </div>
+                    <div class="mp-block-desc__block">
+                        <div class="mp-block-desc__block__image"></div>
+                    </div>
+                </div>
+            </div>
         </div>
 
-
-
-
-
-
-
-        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>
+        <br> <br> <br> <br> <br> <br> <br> <br> <br>
     </div>
 
 <?php
