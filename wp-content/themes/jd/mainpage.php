880b39485895e90ecf32a4cac4a471f35d56578d
Konstantin <trigger.kst@gmail.com>
2020-03-19 12:28:40 +0600

    next

--- wp-content/themes/jd/mainpage.php
+++ wp-content/themes/jd/mainpage.php
@@ -20,7 +20,9 @@ get_header();
                 <div class="pattern-block">
                     <div class="pattern-block__in">
                         <div class="pattern-form">
-                            <header class="pattern-block__header">Получите на почту коммерческое предложение без звонка</header>
+                            <header class="pattern-block__header">Получите на почту коммерческое предложение без
+                                звонка
+                            </header>
                             <div class="flex-wrap">
                                 <div class="form">
                                     <div class="form-input">
@@ -36,31 +38,46 @@ get_header();
                                         <input type="submit" value="Получить"/>
                                     </div>
                                 </div>
-                                <div class="label-conf">Все данные защищены <span>и не передаются третьим лицам</span></div>
+                                <div class="label-conf">Все данные защищены <span>и не передаются третьим лицам</span>
+                                </div>
                             </div>
                         </div>
-                        <div class="quote-block"></div>
+                        <div class="quote-block">
+                            <div class="flex-wrap">
+                                <div class="quote-block__title">
+                                    <div class="name">Николай Казанцев</div>
+                                    <div class="title">ЖД Учет</div>
+                                    <div class="social-links">
+                                        <a href="#" class="vk"></a>
+                                        <a href="#" class="fb"></a>
+                                        <a href="#" class="ig"></a>
+                                    </div>
+                                </div>
+                                <div class="quote-block__image"></div>
+                                <div class="quote-block__quote">
+                                    <p>ЖД Учет — надежный отдел регистрации железнодорожного подвижного состава.</p>
+                                    <p>Наша деятельность базируется на трех принципах — надежность, оперативность,
+                                        эффективность.</p>
+                                    <p>Обратившись к нам, вы получите быстрое, профессиональное и надежное
+                                        обслуживание.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
+        <div class="block-3">
+
+        </div>
+
+
+
+
+
+
+
+        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>        <br>
     </div>
 
 <?php
